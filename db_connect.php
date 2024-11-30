dc69152c5b4e66126b83d97d70b6830008124459
Katsura6 <deathead09777@gmail.com>
2024-12-01 00:17:49 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php 
     
-    $servername = "localhost";
+    $servername = "https://6c0c-110-54-223-237.ngrok-free.app/dashboard/";
     $username = "root";        // Default MySQL username
     $password = "";            // Default MySQL password (usually blank for localhost)
     $database = "inv_db";
